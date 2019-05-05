cd13247d386ea07e9c525f5a5afd383d381461d2
krypton <maxdodon25@gmail.com>
2019-05-05 21:11:04 +0300

    updated Image import path

--- src/main/java/com/krypton/snetwork/repository/ImageRepository.java
+++ src/main/java/com/krypton/snetwork/repository/ImageRepository.java
@@ -1,6 +1,6 @@
 package com.krypton.snetwork.repository;
 
-import com.krypton.snetwork.model.Image;
+import com.krypton.snetwork.model.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
