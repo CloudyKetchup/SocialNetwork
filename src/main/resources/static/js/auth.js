5071ba33caa8bb39a9ebc52ce5524310f6f74e71
krypton17 <maxdodon25@gmail.com>
2019-05-09 07:57:10 -0700

    separated javascript for authentication

---
+++ src/main/resources/static/js/auth.js
@@ -0,0 +1,107 @@
+function chooseAvatar() {
+	$('#choose-profile-photo').click()
+}
+function chooseBackground() {
+	$('#choose-background-photo').click()
+}
+// change profile photo to choosen one
+async function changeAvatar() {
+	const reader = new FileReader()
+
+	reader.onload = async (e) => $('.profile-photo').attr('src', e.target.result)
+
+	reader.readAsDataURL($('#choose-profile-photo').prop('files')[0])
+}
+// change profile background to choosen one
+async function changeBackground() {
+	const reader = new FileReader()
+
+	reader.onload = async (e) => $('.profile-background').attr('src', e.target.result)
+
+	reader.readAsDataURL($('#choose-background-photo').prop('files')[0])
+}
+function loginTrigger() {
+  const email    = $('#login-email')
+  const password = $('#login-password')
+  if (email.val() !== '' && password.val() !== '') {
+    // login procedure
+    login(email,password)
+  }
+}
+// login request
+function login(emailfield,passwordfield) {
+  $.ajax({
+    type : 'POST',
+    contentType : 'application/json; charset=utf-8',
+    url : 'http://localhost:8080/login',
+    data : JSON.stringify({
+      'email': emailfield.val(),      // email value from field
+      'password': passwordfield.val() // password value from field
+    }),
+    success : (result) => {
+    	console.log(result)
+	    // check if login was successful
+		if(result['response'] === 'login success') {
+		    // create cookie with user data : name, surname, email, password
+		    setCookie(result['account'])
+		    // redirect to home page
+		    window.location.replace('/')
+		}else if (result['response'] === 'wrong password') {
+		    // empty fields
+		    emailfield.val('')
+		    passwordfield.val('')
+		}
+    }
+  })
+}
+function registerTrigger() {
+  // register form fields
+  const name 	 = $('.name-field')
+  const surname  = $('.surname-field')
+  const email    = $('.email-field')
+  const password = $('.password-field')
+  
+  register(name, surname, email, password)
+}
+// registration request
+function register(name, surname, email, password) {
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json; charset=utf-8',
+		url  : 'http://localhost:8080/register',
+		data : JSON.stringify({
+			'name'	  : name.val(),
+		 	'surname' : surname.val(),
+		  	'email'   : email.val(),
+	      	'password': password.val()
+	   	}),
+	    success : (result) => {
+		    // check if registration was successful
+		    if (result === 'registered') {
+		        sendPhoto(email.val(), 'profile')
+		        sendPhoto(email.val(), 'background')
+		    }else {
+		        // empty fields if failed
+		        name.val('')
+		        surname.val('')
+		        email.val('')
+		        password.val('')
+		    }
+		    // redirect to homepage
+		    window.location.replace("http://localhost:8080/")
+	    }
+  	})
+}
+// send photo via ajax,profile or background
+function sendPhoto(email, photoType) {
+	const formData = new FormData()
+  	formData.append('picture', $('#choose-' + photoType + '-photo').prop('files')[0])
+  	formData.append('email', email)
+	$.ajax({
+		type : 'POST',
+		url  : 'http://localhost:8080/image/add/user/' + photoType + '-picture',
+		contentType : false,
+    	processData : false,
+    	data : formData
+	})
+}
