7bcf66d45d2dd80b34d23302b75560b312993abb
krypton17 <maxdodon25@gmail.com>
2019-05-13 08:48:32 -0700

    added getPosts mapping

--- src/main/java/com/krypton/snetwork/controllers/GroupController.java
+++ src/main/java/com/krypton/snetwork/controllers/GroupController.java
@@ -2,9 +2,10 @@ package com.krypton.snetwork.controllers;
 
 import com.krypton.snetwork.model.group.Group;
 import com.krypton.snetwork.model.user.User;
+import com.krypton.snetwork.model.common.Post;
 import com.krypton.snetwork.service.group.GroupServiceImpl;
+import com.krypton.snetwork.service.image.ImageServiceImpl;
 import com.krypton.snetwork.service.user.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,9 +19,12 @@ public class GroupController {
 
     private final UserServiceImpl userService;
 
-    public GroupController(GroupServiceImpl groupService, UserServiceImpl userService) {
+    private final ImageServiceImpl imageService;
+
+    public GroupController(GroupServiceImpl groupService, UserServiceImpl userService, ImageServiceImpl imageService) {
         this.groupService = groupService;
         this.userService  = userService;
+        this.imageService = imageService;
     }
     /**
      * get {@link Group} by id
@@ -33,7 +37,7 @@ public class GroupController {
         return groupService.getGroup(id);
     }
     /**
-     * get {@link Group} by name,useful for search purpose
+     * get {@link Group} by name,used for search purpose
      *
      * @param name      {@link Group} name
      */
@@ -41,6 +45,22 @@ public class GroupController {
     public Group getGroup(@PathVariable("name") String name) {
         return groupService.getGroup(name);
     }
+    /**
+     * get {@link Group} {@link Post}'s set
+     *
+     * @param id        {@link Group} id
+     * @return {@link Post}'s
+     */
+    @GetMapping("/posts/{id:.+}")
+    public Set<Post> getPosts(@PathVariable("id") Long id) {
+        return groupService.getGroup(id).getPosts();
+    }
+    /**
+     * get {@link Group} followers
+     *
+     * @param id        {@link Group} id
+     * @return {@link User}'s set
+     */
     @GetMapping("/followers/{id:.+}")
     public Set<User> getFollowers(@PathVariable("id") Long id) {
         return groupService.getGroup(id).getFollowers();
@@ -67,7 +87,7 @@ public class GroupController {
         }else {
             Group group = groupService.createGroup(name, admin);
 
-            groupService.saveProfileAndBackgroundPicture(
+            imageService.saveProfileAndBackgroundPicture(
                     group,
                     profilePhoto,
                     background
